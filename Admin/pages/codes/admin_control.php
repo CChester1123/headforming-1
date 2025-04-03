bf8d102ad6bfd99b2b050e3c1233f6de64d55962
Doofens24 <doommail24@gmail.com>
2025-04-03 11:23:49 +0800

    try

--- Admin/pages/codes/admin_control.php
+++ Admin/pages/codes/admin_control.php
@@ -702,6 +702,5 @@ else if ($pick == 24 || $pick == "24" || $pick == '24') {
 	$resistanceInpection  = htmlentities(htmlspecialchars(urldecode($_POST['resistanceInpection'])));
 	$remarksResistance  = htmlentities(htmlspecialchars(urldecode($_POST['remarksResistance'])));
 
-
 	echo $sql = $insertdata->updateThermalList($prod_id, $status, $workorder, $date, $time, $shift, $operatorName, $teamLead, $machineNo, $product, $type, $InspectedBY, $maintenancecheced, $handle, $substrate, $handleTreeColor, $substrateLotNum, $handleTreeMaterialNum, $texwipeLogo, $remarksInprocess, $arrTemp, $actTempUpnLow, $TempUpnLow, $arrHeat, $actHeatingTime, $HeatingTime, $arrSwab, $actSwabHandleFixture, $SwabHandleFixture, $arrFixture, $actFixtureClosingTime, $FixtureClosingTime, $productionStats, $remarksProduction, $visualInpection, $remarksVisual, $resistanceInpection, $remarksResistance);
 }
