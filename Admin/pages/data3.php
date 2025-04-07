229b03eb4a3a441ef9fa2bba76e156484b63fe33
Doofens24 <doommail24@gmail.com>
2025-04-07 09:41:47 +0800

    Try: Excel Swab

--- Admin/pages/data3.php
+++ Admin/pages/data3.php
@@ -7,7 +7,13 @@
         <td>Date</td>
         <td>Machine Number</td>
         <td>Quality By</td>
-        <td>Pull Testing</td>
+        <td>Pull Testing (Min)</td>
+        <td>Sample 1</td>
+        <td>Sample 2</td>
+        <td>Sample 3</td>
+        <td>Sample 4</td>
+        <td>Sample 5</td>
+        <td>Passed/Failed</td>
     </tr>
 
     <?php
@@ -15,6 +21,8 @@
     $i = 1;
     $rows = mysqli_query($conn2, "SELECT * FROM tbl_swabassembly WHERE YEAR(STR_TO_DATE(date, '%M %d, %Y')) = $yearSelected AND (status != '0' OR status IS NULL)");
     foreach ($rows as $row):
+
+        $pullTestingSamples = explode(',', $row['pulltestingSample']);
     ?>
         <tr>
             <td> <?php echo $i++; ?> </td>
@@ -23,7 +31,13 @@
             <td> <?php echo $row["date"]; ?> </td>
             <td> <?php echo $row["machineNo"]; ?> </td>
             <td> <?php echo $row["InspectedBY"]; ?> </td>
-            <td> <?php echo $row["pulltestingMin"]; ?> (<?php echo $row["pulltestingSample"]; ?>) <?php echo $row["remarksPullTesting"]; ?> </td>
+            <td> <?php echo $row["pulltestingMin"]; ?> </td>
+            <td> <?php echo $pullTestingSamples[0]; ?> </td>
+            <td> <?php echo $pullTestingSamples[1]; ?> </td>
+            <td> <?php echo $pullTestingSamples[2]; ?> </td>
+            <td> <?php echo $pullTestingSamples[3]; ?> </td>
+            <td> <?php echo $pullTestingSamples[4]; ?> </td>
+            <td> <?php echo $row["remarksPullTesting"]; ?> </td>
         </tr>
     <?php endforeach; ?>
 </table>
